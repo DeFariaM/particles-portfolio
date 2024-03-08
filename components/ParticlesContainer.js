1d5c8e29db8bfa008e794dd07850dd6ecd390430
MDeFaria <maria.defariasf@gmail.com>
2024-03-08 12:27:48 -0400

    add: particles config

--- components/ParticlesContainer.js
+++ components/ParticlesContainer.js
@@ -1,5 +1,103 @@
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import React, { useState, useEffect } from "react";
+import { loadSlim } from "@tsparticles/slim";
+
 const ParticlesContainer = () => {
-  return <div>Particles Container</div>;
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
+  return (
+    init && (
+      <Particles
+        className="absolute h-full w-full"
+        id="tsparticles"
+        particlesLoaded={particlesLoaded}
+        options={{
+          background: {
+            color: {
+              value: "",
+            },
+          },
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: false,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 90,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#e68e2e",
+            },
+            links: {
+              color: "#f5d393",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            /*  collisions: {
+            enable: true,
+          }, */
+            move: {
+              directions: "none",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
+              random: false,
+              speed: 1,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 150,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+    )
+  );
 };
 
 export default ParticlesContainer;
