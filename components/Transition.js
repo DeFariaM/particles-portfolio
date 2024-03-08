a4d869c94025410c2b7f32a145674d6cc864e88a
MDeFaria <maria.defariasf@gmail.com>
2024-03-07 22:40:07 -0400

    add: transition animation between pages

--- components/Transition.js
+++ components/Transition.js
@@ -1,7 +1,49 @@
+//framer motion
+import { motion } from "framer-motion";
+
+//variants
+const transitionVariants = {
+  initial: {
+    x: "100%",
+    width: "100%",
+  },
+  animate: {
+    x: "0%",
+    width: "0%",
+  },
+  exit: {
+    x: ["0%", "100%"],
+    width: ["0%", "100%"],
+  },
+};
+
 const Transition = () => {
   return (
     <>
-      <div>Transition</div>
+      <motion.div
+        variants={transitionVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{ delay: 0.2, duration: 0.6, ease: "easeInOut" }}
+        className="fixed bottom-0 right-full top-0 z-30 h-screen w-screen bg-[#2e2257]"
+      ></motion.div>
+      <motion.div
+        variants={transitionVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{ delay: 0.4, duration: 0.6, ease: "easeInOut" }}
+        className="fixed bottom-0 right-full top-0 z-20 h-screen w-screen bg-[#3b2d71]"
+      ></motion.div>
+      <motion.div
+        variants={transitionVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{ delay: 0.6, duration: 0.6, ease: "easeInOut" }}
+        className="fixed bottom-0 right-full top-0 z-10 h-screen w-screen bg-[#4b3792]"
+      ></motion.div>
     </>
   );
 };
