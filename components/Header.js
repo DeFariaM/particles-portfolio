870dd8c2d9b5a03521190428f613519581c4e4eb
MDeFaria <maria.defariasf@gmail.com>
2024-03-08 14:14:21 -0400

    add: header, logo and social links

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,36 @@
+//next img
+import Image from "next/image";
+
+//next link
+import Link from "next/link";
+
+//components
+import Socials from "./Socials";
+
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header
+      className="absolute z-30 flex w-full items-center px-16 xl:h-[90px] xl:px-0
+    "
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col items-center justify-between py-6 lg:flex-row">
+          {/* logo */}
+          <Link href={"/"}>
+            <Image
+              src={"/logo.svg"}
+              width={300}
+              height={30}
+              alt=""
+              priority={true}
+            />
+          </Link>
+          {/* socials */}
+          <Socials />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,28 @@
+//links
+import Link from "next/link";
+
+//icons
+import { RiLinkedinBoxFill, RiGithubFill } from "react-icons/ri";
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-x-5 text-3xl">
+      <Link
+        href={"https://github.com/DeFariaM"}
+        target="blank_"
+        className="transition-all duration-300 hover:text-accent"
+      >
+        <RiGithubFill />
+      </Link>
+      <Link
+        href={"https://www.linkedin.com/in/mariadefaria/"}
+        target="blank_"
+        className="transition-all duration-300 hover:text-accent"
+      >
+        <RiLinkedinBoxFill />
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
