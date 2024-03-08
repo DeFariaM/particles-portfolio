aa0b430b2fd06236fb053a2aa66e33fc36ad5c84
MDeFaria <maria.defariasf@gmail.com>
2024-03-07 21:50:23 -0400

    add: layout config

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,28 @@
-const Layout = () => {
-  return <div>Layout</div>;
+//fonts
+import { Sora } from "@next/font/google";
+
+//font config
+const sora = Sora({
+  subsets: ["latin"],
+  variable: "--font-sora",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+});
+
+//components
+import Nav from "./Nav";
+import Header from "./Header";
+import TopLeftImg from "./TopLeftImg";
+
+const Layout = ({ children }) => {
+  return (
+    <div
+      className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative`}>
+      <TopLeftImg />
+      <Nav />
+      <Header />
+      {children}
+    </div>
+  );
 };
 
 export default Layout;
