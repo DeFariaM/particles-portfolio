f383d61044a7cf2d16bfcb826ad97852c19e31dd
MDeFaria <maria.defariasf@gmail.com>
2024-03-08 12:28:17 -0400

    add: projects btn

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -1,5 +1,30 @@
+//next img
+import Image from "next/image";
+
+//next link
+import Link from "next/link";
+
+//icons
+import { HiArrowRight } from "react-icons/hi2";
+
 const ProjectsBtn = () => {
-  return <div>Project Btn</div>;
+  return (
+    <div className="mx-auto max-w-fit xl:mx-0">
+      <Link
+        href={"/work"}
+        className="relative flex h-[185px] w-[185px] items-center justify-center bg-circleStar bg-cover bg-center bg-no-repeat"
+      >
+        <Image
+          src={"/rounded-text.png"}
+          width={141}
+          height={148}
+          alt=""
+          className="h-full max-h-[148px] w-full max-w-[141px] animate-spin-slow"
+        />
+        <HiArrowRight className="absolute text-4xl transition-all duration-300 group-hover:translate-x-2" />
+      </Link>
+    </div>
+  );
 };
 
 export default ProjectsBtn;
