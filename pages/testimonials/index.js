6eab630e4cd159386df6dae42f7c3303f5d8e713
MDeFaria <maria.defariasf@gmail.com>
2024-03-20 19:02:40 -0400

    add: page of testimonials

--- pages/testimonials/index.js
+++ pages/testimonials/index.js
@@ -1,5 +1,38 @@
+//components
+import TestimonialSlider from "../../components/TestimonialSlider";
+
+//framer motion
+import { motion } from "framer-motion";
+
+//variants
+import { fadeIn } from "../../variants";
+
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return (
+    <div className="h-full bg-primary/30 py-32 text-center">
+      <div className="container mx-auto flex h-full flex-col justify-center">
+        {/* title */}
+        <motion.h2
+          variants={fadeIn("up", 0.2)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          className="h2 mb-8 xl:mb-0"
+        >
+          What clients <span className="text-accent">say.</span>
+        </motion.h2>
+        {/* slider */}
+        <motion.div
+          variants={fadeIn("up", 0.4)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+        >
+          <TestimonialSlider />
+        </motion.div>
+      </div>
+    </div>
+  );
 };
 
 export default Testimonials;
