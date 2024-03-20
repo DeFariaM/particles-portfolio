0a6ac21b63e373d46affb99ba1356a2b2beaabcf
MDeFaria <maria.defariasf@gmail.com>
2024-03-20 19:00:53 -0400

    add: styles and background images for decoration

--- components/Bulb.js
+++ components/Bulb.js
@@ -1,5 +1,18 @@
+//next img
+import Image from "next/image";
+
 const Bulb = () => {
-  return <div>Bulb</div>;
+  return (
+    <div className="absolute -bottom-12 -left-36 z-10 w-[200px] rotate-12 animate-pulse mix-blend-color-dodge duration-75 xl:w-[260px]">
+      <Image
+        src={"/bulb.png"}
+        width={260}
+        height={200}
+        className="h-full w-full"
+        alt=""
+      />
+    </div>
+  );
 };
 
 export default Bulb;
--- components/Circles.js
+++ components/Circles.js
@@ -1,5 +1,21 @@
+//next image
+import Image from "next/image";
+
 const Circles = () => {
-  return <div>Circles</div>;
+  return (
+    <div
+      className="absolute -bottom-2 -right-16 z-10 w-[200px] animate-pulse
+    mix-blend-color-dodge duration-75 xl:w-[300px]"
+    >
+      <Image
+        src={"/circles.png"}
+        width={260}
+        height={200}
+        className="h-full w-full"
+        alt=""
+      />
+    </div>
+  );
 };
 
 export default Circles;
