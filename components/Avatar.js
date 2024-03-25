3b1bdac8940b9d84fc40696861c46bd1a0b0f266
MDeFaria <maria.defariasf@gmail.com>
2024-03-25 19:53:00 -0400

    fix: change image of the avatar

--- components/Avatar.js
+++ components/Avatar.js
@@ -5,9 +5,9 @@ const Avatar = () => {
   return (
     <div className="hidden xl:flex xl:max-w-none">
       <Image
-        src={"/avatar.png"}
-        width={737}
-        height={678}
+        src={"/astro.png"}
+        width={637}
+        height={578}
         alt=""
         className="translate-z-0 h-full w-full"
       />
