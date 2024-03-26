c54023c44e7657bd5163c1fa62d311a399e0910b
MDeFaria <maria.defariasf@gmail.com>
2024-03-26 19:50:08 -0400

    add: dictionaries in english and spanish

---
+++ public/locales/en.js
@@ -0,0 +1,32 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  home: {
+    title: "Transforming ideas",
+    title2: "Into",
+    titleAccent: "Digital Reality",
+    paragraph1:
+      "I'm a FullStack Web Developer but my true passion is the FrontEnd development.",
+    paragraph2:
+      "My passion for art and technology are the perfect combination to make the app of your dreams come true.",
+  },
+  about: {
+    title: "Captivating",
+    titleAccent: "stories",
+    title2: "birth magnificient designs.",
+    paragraph:
+      " 6 years ago I was a graphic design student, but, being Venezuelan, the situation in the country forced me to leave the country and, consequently, abandon my degree. In the last year I made the decision to take that passion for art and transform it into web apps.",
+  },
+  services: {
+    title: "My services",
+    paragraph1:
+      "Although I am a certified as a FullStack Web Developer, I am constantly learning how to improve my work.",
+    paragraph2:
+      "I am currently studying UX/UI design and SEO to provide a better service.",
+  },
+  work: {
+    title: "My work",
+    paragraph1:
+      "I have been working on personal and academic projects to improve my skills as a developer. I use JavaScript frameworks such as React, Next, TailwindCSS, Node and Express",
+    paragraph2: "Currently learning UX/UI design.",
+  },
+};
---
+++ public/locales/es.js
@@ -0,0 +1,33 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  home: {
+    title: "Transformando ideas",
+    title2: "En",
+    titleAccent: "Realidad Digital",
+    paragraph1:
+      "Soy Desarrolladora Web FullStack, pero, mi verdadera pasión es el desarrollo FrontEnd.",
+    paragraph2:
+      "Mi pasión por el arte y la tecnología son la combinación perfecta para hacer realidad la app de tus sueños.",
+  },
+
+  about: {
+    title: "Historias",
+    titleAccent: "cautivadoras",
+    title2: "dan lugar a diseños increíbles.",
+    paragraph:
+      "Hace 6 años, era estudiante de diseño gráfico, pero, siendo venezolana, la situación país me forzó a dejar el país y, en consecuencia, abandonar la carrera. En el último año, tomé la decisión de tomar mi pasión por el arte y transformarlo en aplicaciones web.",
+  },
+  services: {
+    title: "Mis servicios",
+    paragraph1:
+      "Aunque estoy certificada como Desarrolladora Web FullStack, estoy constantemente buscando maneras de mejorar mi trabajo.",
+    paragraph2:
+      "Actualmente me encuentro estudiando diseño UX/UI y SEO para brindar un mejor servicio.",
+  },
+  work: {
+    title: "Mis trabajos",
+    paragraph1:
+      "He trabajado en proyectos personales y académicos para mejorar mis habilidades como desarrolladora. Utilizo frameworks de JavaScript como React, Next, TailwindCSS, Node y Express.",
+    paragraph2: "Actualmente estoy aprendiendo diseño UX/UI.",
+  },
+};
